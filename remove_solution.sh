58fc626b28432735fa01c7f353c50ef90152f0ab
Amir Sanjar <afsanjar@gmail.com>
2017-01-24 12:31:14 -0600

    update remove

--- remove_solution.sh
+++ remove_solution.sh
@@ -1,49 +1,37 @@
 #!/bin/bash
+#set -x
 usage() {
     echo "usage: $(basename $0) --sd <solution definition file name> --spark-version <spark version>"
-    echo "    where:"
-    echo "        <spark version> is one of [\"1.6.2\", \"2.1\"]"
     exit 1;
 }
 
-while [ ! -z $4 ]; do
+while [ ! -z $2 ]; do
     case "$1" in
         --sd ) solution_def_file=$2 ;;
-        * ) usage ;;
-    esac
-    case "$3" in
-        --spark-version ) SPARK_VERSION=$4 ;;
         * ) usage ;;
     esac
     shift
 done
 echo "solution definition file="$solution_def_file
-echo "spark version = "$SPARK_VERSION
-if [ -z $SPARK_VERSION ] || [ -z $solution_def_file ]; then
-    usage
-fi
 
 remove_service(){
 	service_name=$1
         dep_service=$2
 	bd_ip=$3
 	bd_user=$4
-	namenode=$5
-	resourcemanager=$6
-	sparkmaster=$7
 	server=root@$bd_ip
         
 
 	ssh $server "$service_name/stop.sh" < /dev/null
 	ssh $server "$service_name/cleanup.sh" < /dev/null
-	ssh $server rm -rf $service_name
+	ssh $server rm -rf $service_name < /dev/null
     if [ ! -z $dep_service ] ; then
               	ssh $server "$dep_service/stop.sh" < /dev/null
-               	ssh $server "$dep_service/cleanup.sh" < /dev/null        	
+               	ssh $server "$dep_service/cleanup.sh"     < /dev/null 	
 	 fi
 }
 
-while IFS=',' read -r f1 f2 f3 f4 f5 f6 f7
+while IFS=',' read -r f1 f2 f3 f4 f5 f6 f7 
 do 
   ## Ignore lines start with "#"
   case $f1 in
@@ -55,7 +43,8 @@ do
   echo "  Service Location="$f3
   echo "  Service User Name="$f4
   echo "************************************** "
-remove_service $f1 $f2 $f3 $f4 $f5 $f6 $f7 
+remove_service $f1 $f2 $f3 $f4 
+ 
 done < "$solution_def_file"
 ./solution_status.sh --sd $solution_def_file
 
